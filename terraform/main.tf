63b8b8a97250b8a435bc11854d1caab422b96ee9
HaMinhQuan-Uit <haminhquan091203@gmail.com>
2026-08-09 23:45:15 +0700

    fix: add abort incomplete multipart upload to lifecycle

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,9 @@ resource "aws_s3_bucket_lifecycle_configuration" "app_data" {
     noncurrent_version_expiration {
       noncurrent_days = 90
     }
+ abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+	}
   }
 }
 
